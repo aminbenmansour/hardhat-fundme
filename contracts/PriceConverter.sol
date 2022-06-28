bbc3d5aa23eae35c8382821039d52a5889119092
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-06-28 18:21:25 +0100

    returning the price of 1 unit of eth to usd

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -12,5 +12,10 @@ library PriceConverter {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(
             0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
         );
+
+        (, int256 answer, , , ) = priceFeed.latestRoundData();
+        
+        // ETH/USD rate in 18 digit
+        return uint256(answer * 1e10);
     }
 }
