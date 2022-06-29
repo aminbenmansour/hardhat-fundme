75bc7c65d51c81e6c4d760f50237fce59c5009bb
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-06-29 03:01:09 +0100

    switching to rinkeby testnet instead of ropsten

--- hardhat.config.js
+++ hardhat.config.js
@@ -26,8 +26,8 @@ module.exports = {
   solidity: "0.8.4",
   defaultNetwork: "hardhat",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+    rinkeby: {
+      url: process.env.RINKEBY_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
