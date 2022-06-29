e9f26d3f2e413f2f944ed77e086c18a241a9527e
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-06-29 03:32:45 +0100

    dynamically change chainlink provider address depending on the chain

--- deploy/01-deploy-fundme.js
+++ deploy/01-deploy-fundme.js
@@ -1,10 +1,20 @@
 const { getNamedAccounts, deployments, network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
 
 module.exports =async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
+    // what happens when we want to change chains ?
     // when going for localhost or hardhat network we want to use a mock
 
+    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [
+            // price feed address
+            ethUsdPriceFeedAddress
+        ]
+    })
 }
---
+++ helper-hardhat-config.js
@@ -0,0 +1,16 @@
+// inspired from AAVE protocol, from this repo: https://github.com/aave/aave-v3-core/blob/master/helper-hardhat-config.ts
+
+const networkConfig = {
+    4: {
+        name: "rinkeby",
+        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e"
+    },
+    137: {
+        name: "polygon",
+        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945"
+    }
+}
+
+module.export = {
+    networkConfig,
+}
