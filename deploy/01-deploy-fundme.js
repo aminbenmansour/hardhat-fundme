d0ae6533df5329b746187a0c30d085de64b205eb
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-06-30 01:25:17 +0100

    programmatically verify on etherscan

--- deploy/01-deploy-fundme.js
+++ deploy/01-deploy-fundme.js
@@ -1,6 +1,8 @@
 const { getNamedAccounts, deployments, network } = require("hardhat")
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 
+const { verify } = require("../utils/verify")
+
 module.exports =async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
@@ -18,17 +20,20 @@ module.exports =async ({ getNamedAccounts, deployments }) => {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
     
+    const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: [
-            ethUsdPriceFeedAddress
-        ],
+        args: args,
         log: true,
         // we need to wait if on a live network so we can verify properly
         waitConfirmations: network.config.blockConfirmations || 1,
     })
 
     log("--------------------------------------------")
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        await verify(fundMe.address, args)
+    }
 }
 
 module.exports.tags = ["all", "fundme"]
