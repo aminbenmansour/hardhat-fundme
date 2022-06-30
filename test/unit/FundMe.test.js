2ceec42f4bbba3cafb74a9c9b789c958458cf8fc
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-06-30 03:02:56 +0100

    passing test on constructor

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,16 +1,31 @@
+const { assert } = require("chai");
 const { deployments, ethers, getNamedAccounts } = require("hardhat");
 
-describe("FundMe !", async () => {
-    let fundMe;
-    BeforeEach(async () => {
+describe("FundMe", async () => {
+    let fundMe
+    let deployer
+    let mockV3Aggregator
 
-        //getting contract owner
-        const { deployer } = await getNamedAccounts()     
+    beforeEach(async () => {
+        // getting contract owner
+        deployer = (await getNamedAccounts()).deployer     
         
         // deploy fundme and associate it to the deployer with hardhat-deploy
         await deployments.fixture(["all"])
         fundMe = await ethers.getContract("FundMe", deployer)
 
+        // MockV3Aggregator
+        mockV3Aggregator = await ethers.getContract(
+            "MockV3Aggregator",
+            deployer
+        )
 
+    })
+
+    describe("constructor", function() {
+        it("sets the aggregator address correctly", async () => {
+            const response = await fundMe.priceFeed()
+            assert.equal(response, mockV3Aggregator.address)
+        })
     })
 })
