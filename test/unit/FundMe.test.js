f525b54066ffbdf43c93663240b05fed6f9ab6d8
Amine Ben Mansour <amin.benmansour.10@gmail.com>
2022-06-30 10:04:52 +0100

    passing tests on fund, both ways!

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -1,10 +1,11 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { deployments, ethers, getNamedAccounts } = require("hardhat");
 
 describe("FundMe", async () => {
     let fundMe
     let deployer
     let mockV3Aggregator
+    const sendValue = ethers.utils.parseEther("1")
 
     beforeEach(async () => {
         // getting contract owner
@@ -26,6 +27,19 @@ describe("FundMe", async () => {
         it("sets the aggregator address correctly", async () => {
             const response = await fundMe.priceFeed()
             assert.equal(response, mockV3Aggregator.address)
+        })
+    })
+
+    describe("fund", async () => {
+        it("Fails if you don't send enough ETH", async () => {
+            await expect(fundMe.fund()).to.be.reverted
+        })
+
+        it("Update the amount of ETH, TX should be accepted", async () => {
+            await fundMe.fund({value: sendValue})
+            const response = await fundMe.addressToAmountFunded(deployer)
+
+            assert.equal(response.toString(), sendValue.toString())
         })
     })
 })
